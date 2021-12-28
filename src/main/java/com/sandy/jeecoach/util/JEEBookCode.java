5afc0465578c3ba3d402850b9f37e8b32d3b717f
Sandeep Deb <deb.sandeep@gmail.com>
2021-12-28 22:15:26 +0530

    First commit

---
+++ src/main/java/com/sandy/jeecoach/util/JEEBookCode.java
@@ -0,0 +1,7 @@
+package com.sandy.jeecoach.util;
+
+public class JEEBookCode {
+
+    public static final String PEARSON_IIT_FOUNDATION = "PF" ;
+    public static final String MTG_REASONING          = "MR" ;
+}
